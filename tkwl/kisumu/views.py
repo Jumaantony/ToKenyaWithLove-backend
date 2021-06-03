dfa6d1ded0672f64939e36178c173dba8818f81b
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-06-03 21:00:22 +0300

    added pagination in blog list

--- tkwl/kisumu/views.py
+++ tkwl/kisumu/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Cause, BlogPost
+from django.core.paginator import *
 
 
 # Create your views here.
@@ -26,10 +27,22 @@ class CauseDetail(DetailView):
 
 def blog_list(request):
     posts = BlogPost.published.all()
+    p = Paginator(posts, 3)  # paginator object
+    # getting the desired page number from url
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page number is not an integer then assign the first page
+        page_obj = p.page(1)
+    except EmptyPage:
+        # if page is empty then return the last page
+        page_obj = p.page(p.num_pages)
 
+    context = {'posts': page_obj}
     return render(request,
                   'blog_list.html',
-                  {'posts': posts})
+                  context)
 
 
 def blog_detail(request, year, month, day, post):
